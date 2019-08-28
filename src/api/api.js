12cffceeb899d5a3d31781f53a1c3ab93329d625
hub <hub@infoearth.com>
2019-08-28 22:22:14 +0800

    提交代码

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+let base = '/api';//http://192.168.1.4:8080
+
+export const requestLogin = params => { return axios.post(`${base}/login/valid`, params).then(res => res.data); };
+
+export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+
+export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+
+export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+
+export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+
+export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+
+export const addUser = params => { return axios.post(`${base}/user/add`, { params: JSON.stringify(params) }); };
---
+++ src/views/nav3/Page6.vue
@@ -0,0 +1,44 @@
+<template>
+  <!--第一课-->
+  <div id="vue-app">
+    <h5>属性:{{name}}</h5>
+    <p>方法调用：{{loadData()}}</p>
+    <a v-bind:href="website">百度一下</a>
+    <div v-html="websiteTag"></div>
+    <input type="text"  v-bind:placeholder='bindData'/>
+  </div>
+  <!--第二课-->
+</template>
+  <script>
+  import { mapGetters } from 'vuex'
+  import { mapActions } from 'vuex'
+  import axios from 'axios'
+  
+  export default {
+	  data()
+	  {
+		  return {
+		  name:"Hello World!!!",
+		  website:"https://www.baidu.com",
+		  bindData:"请输入正确信息",
+		  websiteTag:"<a href='https://www.baidu.com'>百度一下</a>",
+	  }},
+	  methods:{
+			loadData:function(){
+				  const Url ="http://221.0.170.174:8049/AjaxHandler.ashx?class=AjaxProjectManage&method=GetProjectActivity&ProjectID=ac3a0e8c-db84-4d5a-8320-7f1042045658";
+				  return axios.get(Url).then((res) => {console.log(res+"返回值"); return res;}).catch((error) => {console.log(error.response+"错误信息");})
+                // this.$http.get(url).then(function(data){
+                //   console.info(data);
+                //     // var json=data.body;
+                //     // this.data=eval("(" + json +")");
+                // },function(response){
+                //     console.info(response);
+				// })
+				// $.get(url,function(data){
+                //     _self.data=eval("(" + data +")");
+                // })
+				// return "这是方法测试";
+			}
+		}
+	}
+    </script>
